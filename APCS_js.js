ab05c9af018f5f63e7ddb1ede595b0571c5c0e44
samanthask8s <samanthask8s@gmail.com>
2016-05-18 09:30:20 -0700

    Add files via upload

--- APCS_js.js
+++ APCS_js.js
@@ -1,17 +1,21 @@
 function hello(){
   var x;
-  var r = confirm("Press a button");
+  var r = confirm("Do you want to see a picture of a koala?");
   if (r == true) {
-    x = "You pressed OK!";
+    var elem = document.createElement("img");
+    elem.setAttribute("src", "http://www.animalfactguide.com/wp-content/uploads/2013/01/koala2.jpg");
+    elem.setAttribute("height", "450px");
+    document.getElementById("body").appendChild(elem);
   } else {
-    x = "You pressed Cancel!";
+    window.alert("Click again if you change your mind.");
   }
-  window.alert(x);
+
 }
 function math(){
   var sum = prompt("What is the sum of 2 and 3? ");
   if(sum==='5'){
     window.alert("good job!");
+    document.getElementById("sum").innerHTML = "done";
   }else if(sum===null){
     window.alert("ok maybe next time.");
   }else{
